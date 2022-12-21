31e5035cba3dd581f5e787d82f810d8c2d2b972d
José Rodolpho <j.rodolpho29@hotmail.com>
2022-12-21 18:24:56 -0300

    feat: create new user / add avatar to the user's posts

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+users = []
 posts = []
+
+@app.route("signup", methods=["POST"])
+def signup():
+    user = request.get_json()
+    if(user.get("username") == None or user.get("avatar") == None ):
+        return "Informações inválidas", 400
+    users.append(user)
+    return "Usuário criado com sucesso", 201
 
 @app.route("/posts", methods=["GET", "POST"])
 def get_post_posts():
@@ -10,6 +19,8 @@ def get_post_posts():
         return jsonify(posts), 200
     elif request.method == "POST":
         post = request.get_json()
+        if(post.get("username") == None or post.get("tweet") == None):
+            return "Informações inválidas", 400
         if(len(posts) == 10):
             posts.pop()
         posts.insert(0, post)
